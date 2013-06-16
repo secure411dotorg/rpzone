3c9eb75c4d465734d25fb3fc413ae9d2b35cca8f
secure411dotorg <data@serverauthority.net>
2013-06-15 21:03:04 -0300

    Update refresh_drop.sh

--- scripts/refresh_drop.sh
+++ scripts/refresh_drop.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 # PURPOSE: Update the dependencies with
 # current SH Do Not Route or Peer (DROP) as a blocklist
+# NAME: refresh_drop.sh (part of rpzone helper scripts)
+# Copyright (C) 2012 April Lorenzen
 #
+# Paper mail contact: April Lorenzen, PO Box 293, Jamestown RI 02835
+# Electronic contact: https://github.com/secure411dotorg/rpzone/issues
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
 source /opt/process-locking/process-locking-header.sh
 
 DEPENDENCIESDIR="/opt/rpz-dependencies"
