2613749b8089362034c0d3ff95496d6e7dc9c22b
secure411dotorg <data@serverauthority.net>
2013-06-15 20:07:43 -0400

    Update nsupdate_hits.sh

--- scripts/nsupdate_hits.sh
+++ scripts/nsupdate_hits.sh
@@ -5,6 +5,28 @@
 # Intended to run on crontab periodically such as hourly
 #
 # Set response-policy for zone "hits" to DISABLED
+
+# 
+# NAME: nsupdate_hits.sh (part of rpzone helper scripts)
+# Copyright (C) 2012 April Lorenzen
+#
+# Paper mail contact: April Lorenzen, PO Box 293, Jamestown RI 02835
+# Electronic contact: https://github.com/secure411dotorg/rpzone/issues
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+
 # FIXME test if any response-policy is even needed for this zone
 # FIXME add protection from running multiple copies of script
 
